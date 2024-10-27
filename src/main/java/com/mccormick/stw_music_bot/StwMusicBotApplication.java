7031132b67b73ec4bc1b3b837943d01cad319e52
StewardMcCormick <bessonoven@mail.ru>
2024-10-27 20:25:08 +0300

    First commit

---
+++ src/main/java/com/mccormick/stw_music_bot/StwMusicBotApplication.java
@@ -0,0 +1,13 @@
+package com.mccormick.stw_music_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwMusicBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StwMusicBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mccormick/stw_music_bot/StwMusicBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccormick.stw_music_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StwMusicBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
